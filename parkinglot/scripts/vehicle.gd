6af9506c579b22ce676be183647b579ec8b5a819
Shuban Biswas (orbi8r) <157096929+orbi8r@users.noreply.github.com>
2025-02-20 20:37:12 +0530

    initialized project

---
+++ parkinglot/scripts/vehicle.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+# Vehicle Models
+@onready var models: Node3D = $models
+
+
+func _ready() -> void:
+	randomize_model()
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func randomize_model() -> void:
+	# Hides all children
+	for model in models.get_children():
+		model.hide()
+	# Makes a Random Child visible
+	models.get_child(randi_range(0,models.get_child_count()-1)).show()
